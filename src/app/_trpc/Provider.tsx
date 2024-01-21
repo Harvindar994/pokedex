6091792f8d2c0d0af00dff0e2c8ff1b73bc5a021
Harvindar Singh <sjagjeet994@gmail.com>
2024-01-21 11:08:10 +0530

    Update

--- src/app/_trpc/Provider.tsx
+++ src/app/_trpc/Provider.tsx
@@ -5,14 +5,23 @@ import { httpBatchLink } from "@trpc/client";
 import React, { useState } from "react";
 
 import { trpc } from "./client";
+import { DevelopmentMode } from "../config/config";
+
 
 export default function Provider({ children }: { children: React.ReactNode }) {
+  let url=null;
+  if (DevelopmentMode){
+    url = "http://localhost:3000/api/trpc"
+  }else{
+    url = "https://pokedex-amber-tau.vercel.app/api/trpc"
+  }
+
   const [queryClient] = useState(() => new QueryClient({}));
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: `https://pokedex-amber-tau.vercel.app/api/trpc`,
+          url: url,
         }),
       ],
     })
